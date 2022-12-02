0a82608bbb3bc6fb658100d2521212395b899cd0
Rodrigo Castro <roi31@hotmail.com>
2022-12-02 01:21:41 +0100

    Implement search page

--- src/Pages/SearchPage.js
+++ src/Pages/SearchPage.js
@@ -1,7 +1,47 @@
 import { Link } from "react-router-dom"
+import { useState, useEffect } from "react";
+import { useDebounce } from "../useDebounce";
+import { Book } from "../components/Book";
+import * as BooksAPI from "../BooksAPI";
 
 
 export const SearchPage = () => {
+
+  const [searchValue, setSearchValue] = useState("");
+  const [error, setError] = useState(false);
+  const [results, setResults] = useState([]);
+  const debounceValue = useDebounce(searchValue, 400);
+
+  const handleInput = (e) => {
+    setSearchValue(e.target.value);
+  }
+
+  const fetchBooks = async (value) => {
+
+    try {
+      const books = await BooksAPI.search(value, "5");
+      setResults(books);
+
+    } catch (error) {
+      setResults(false);
+      setError(error)
+    }
+  }
+
+  const updateShelf = (book, shelfToUpdate) => {
+    book.shelf = shelfToUpdate;
+    BooksAPI.update(book, shelfToUpdate);
+  };
+ 
+  useEffect(() => {
+    if(!debounceValue){
+      setResults([]);
+    } else {
+      fetchBooks(debounceValue);
+    }
+  
+     
+  }, [debounceValue])
 
   return (
     <div className="search-books">
@@ -12,12 +52,24 @@ export const SearchPage = () => {
             <div className="search-books-input-wrapper">
               <input
                 type="text"
+                value={searchValue}
                 placeholder="Search by title, author, or ISBN"
+                onChange={handleInput}
               />
             </div>
           </div>
           <div className="search-books-results">
-            <ol className="books-grid"></ol>
+            <ol className="books-grid">
+              {error && <p>Error</p>}
+              {
+                !results.length 
+                        ? ""
+                        : results.map(book => (
+                          <Book key={book.id} book={book} onShelfChange={updateShelf}/>
+                          ))
+              }
+              
+            </ol>
           </div>
         </div>
   )
---
+++ src/useDebounce.js
@@ -0,0 +1,15 @@
+import { useEffect, useState } from "react";
+
+export const useDebounce = (value, delay) => {
+  const [debounceValue, setDebounceValue] = useState(value);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setDebounceValue(value), delay || 500);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [value, delay]);
+
+  return debounceValue;
+};
