e072f94128597aa1d5f98c7095268d6fa17faae1
Rodrigo C <rodrigocsastre@gmail.com>
2022-12-05 23:06:52 +0100

    Extract select options and shelves

---
+++ src/bookConfig.js
@@ -0,0 +1,13 @@
+
+export const bookOptions = {
+    currentlyReading: "Currently Reading",
+    wantToRead: "Want to Read",
+    read: "Read",
+    none: "None",
+};
+
+export const shelves = {
+    currentlyReading: "Currently Reading",
+    wantToRead: "Want to Read",
+    read: "Read",
+};
