add7bab9bc638c15a80ce85f582da7851c055d0a
Rodrigo C <rodrigocsastre@gmail.com>
2022-12-05 23:10:41 +0100

    Refactor component to display the right props

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,50 +1,43 @@
+import { bookOptions } from "../bookConfig";
 
-
-export const Book = ({book, onShelfChange}) => {
-  const bookOptions = {
-    currentlyReading: "Currently Reading",
-    wantToRead : "Want to Read",
-    read: "Read",
-    none: "None"
-  };
-
+export const Book = ({ book, onShelfChange }) => {
   const handleChange = (e) => {
     onShelfChange(book, e.target.value);
-  }
+  };
 
   return (
-      <li>
-        <div className="book">
-          <div className="book-top">
-              <div
-                className="book-cover"
-                style={{
-                  width: 128,
-                  height: 193,
-                  backgroundImage:
-                    `url(${book?.imageLinks?.thumbnail})`,
-                }}
-              ></div>
-            <div className="book-shelf-changer">
-              <select value={book?.shelf} onChange={handleChange}>
-                <option value="none" disabled>
-                  Move to...
+    <li>
+      <div className="book">
+        <div className="book-top">
+          <div
+            className="book-cover"
+            style={{
+              width: 128,
+              height: 193,
+              backgroundImage: `url(${book?.imageLinks?.thumbnail})`,
+            }}
+          ></div>
+          <div className="book-shelf-changer">
+            <select
+              value={book.shelf ? book.shelf : "none"}
+              onChange={handleChange}
+            >
+              <option value="move" disabled>
+                "Move to..."
+              </option>
+              {Object.keys(bookOptions).map((option) => (
+                <option key={bookOptions[option]} value={option}>
+                  {bookOptions[option]}
                 </option>
-                { Object.keys(bookOptions).map( option => (
-                  <option key={bookOptions[option]} value={option}>
-                      {bookOptions[option]}
-                  </option>
-                  )
-                )}
-              </select>
-            </div>
+              ))}
+            </select>
           </div>
-          <div className="book-title">{book?.title}</div>
-          { 
-              book?.authors
-                .map(author => <div key={author} className="book-authors">{author}</div>)
-          }
         </div>
-      </li>
-  )
-}
+        <div className="book-title">{book?.title}</div>
+        <div className="book-authors">
+          {book.authors ? book.authors.join(",") : ""}
+        </div>
+      </div>
+    </li>
+  );
+};
