edb5bc0db4e29f6477450778ce135466203674f2
Rodrigo C <rodrigocsastre@gmail.com>
2022-12-05 23:08:31 +0100

    Refactor API fetch and props for book

--- src/Pages/SearchPage.js
+++ src/Pages/SearchPage.js
@@ -5,8 +5,7 @@ import { Book } from "../components/Book";
 import * as BooksAPI from "../BooksAPI";
 
 
-export const SearchPage = ({onShelfChange}) => {
-
+export const SearchPage = ({ bookShelf, onShelfChange }) => {
   const [searchValue, setSearchValue] = useState("");
   const [error, setError] = useState(false);
   const [results, setResults] = useState([]);
@@ -14,63 +13,65 @@ export const SearchPage = ({onShelfChange}) => {
 
   const handleInput = (e) => {
     setSearchValue(e.target.value);
-  }
+  };
 
   const fetchBooks = async (value) => {
-
     try {
-      const books = await BooksAPI.search(value, "5");
+      const books = await BooksAPI.search(value, 10);
+
+      books.forEach((book) => {
+        bookShelf.forEach((bookItem) => {
+          if (bookItem.id === book.id) {
+            book.shelf = bookItem.shelf;
+          }
+        });
+      });
       setResults(books);
-
     } catch (error) {
-      setResults(false);
-      setError(error)
+      setResults([]);
+      setError(error);
     }
-  }
+  };
 
-  // const updateShelf = (book, shelfToUpdate) => {
-  //   book.shelf = shelfToUpdate;
-  //   BooksAPI.update(book, shelfToUpdate);
-  // };
- 
   useEffect(() => {
-    if(!debounceValue){
+    if (!debounceValue) {
       setResults([]);
     } else {
       fetchBooks(debounceValue);
     }
-  
-     
-  }, [debounceValue])
+    // eslint-disable-next-line
+  }, [debounceValue]);
 
   return (
     <div className="search-books">
-          <div className="search-books-bar">
-            <Link to="/" className="close-search">
-              Close
-            </Link>
-            <div className="search-books-input-wrapper">
-              <input
-                type="text"
-                value={searchValue}
-                placeholder="Search by title, author, or ISBN"
-                onChange={handleInput}
-              />
-            </div>
-          </div>
-          <div className="search-books-results">
-            <ol className="books-grid">
-              {error && <p>Error</p>}
-              {
-                !results.length 
-                        ? ""
-                        : results.map(book => (
-                          <Book key={book.id} book={book} onShelfChange={onShelfChange}/>
-                          ))
-              }
-              
-            </ol>
-          </div>
+      <div className="search-books-bar">
+        <Link to="/" className="close-search">
+          Close
+        </Link>
+        <div className="search-books-input-wrapper">
+          <input
+            type="text"
+            value={searchValue}
+            placeholder="Search by title, author, or ISBN"
+            onChange={handleInput}
+          />
         </div>
-  )
-}
+      </div>
+      <div className="search-books-results">
+        <ol className="books-grid">
+          {error && <p>Error</p>}
+          {!results.length
+            ? ""
+            : results.map((book) => (
+                <Book
+                  key={book.id}
+                  book={book}
+                  bookShelf={bookShelf}
+                  onShelfChange={onShelfChange}
+                />
+              ))}
+        </ol>
+      </div>
+    </div>
+  );
+};
