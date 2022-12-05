96b7f53ae49af173502cffc2c5e9239f51ca4a86
Rodrigo C <rodrigocsastre@gmail.com>
2022-12-05 23:09:01 +0100

    Delete comments, remove unnecesary prop drilling

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,53 +1,37 @@
 import { Link } from "react-router-dom";
 import { BookShelf } from "../components/BookShelf";
+import { shelves } from "../bookConfig";
+import { Book } from "../components/Book";
 
-
-export function HomePage({books, onShelfChange}) {
-// const [books, setBooks] = useState([]);
-
-const shelves = {
-  currentlyReading: "Currently Reading",
-  wantToRead : "Want to Read",
-  read: "Read"
-};
-  
-  // useEffect(() => {
-  //   BooksAPI.getAll().then((books) => {
-  //     setBooks(books);
-  //   });    
-  // }, [])
-
-  // const updateShelf = (book, shelfToUpdate) => {
-  //   const updatedBooks = books.filter(b => b.id !== book.id);
-  //   book.shelf = shelfToUpdate;
-    
-  //   setBooks([...updatedBooks, book]);
-  //   BooksAPI.update(book, shelfToUpdate);
-  // };
-
-  
+export function HomePage({ books, onShelfChange }) {
   return (
-      <div className="list-books app">
-            <div className="list-books-title">
-              <h1>MyReads</h1>
-            </div>
-            <div className="list-books-content">
-              <div>
-                {
-                  Object.keys(shelves).map(shelf => (
-                      <BookShelf 
-                        key={shelf} 
-                        shelf={shelf} 
-                        shelfName={shelves[shelf]}
-                        books={books}
-                        onShelfChange={onShelfChange}/>
-                  ))
-                }
-              </div>
-            </div>
-            <div className="open-search">
-              <Link to="/search">Add a book</Link>
-            </div>
+    <div className="list-books app">
+      <div className="list-books-title">
+        <h1>MyReads</h1>
       </div>
-  )
+      <div className="list-books-content">
+        <div>
+          {Object.keys(shelves).map((shelf) => {
+            const booksOnThisShelf = books.filter(
+              (book) => book.shelf === shelf && book.shelf !== "none"
+            );
+            return (
+              <BookShelf key={shelf} shelfName={shelves[shelf]}>
+                {booksOnThisShelf.map((book) => (
+                  <Book
+                    key={book.id}
+                    book={book}
+                    onShelfChange={onShelfChange}
+                  />
+                ))}
+              </BookShelf>
+            );
+          })}
+        </div>
+      </div>
+      <div className="open-search">
+        <Link to="/search">Add a book</Link>
+      </div>
+    </div>
+  );
 }
